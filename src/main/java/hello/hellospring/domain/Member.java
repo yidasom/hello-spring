9ee347960f3c66c7161c13437ee4f9c2f11847af
somlh1212@gmail.com <somlh1212@gmail.com>
2022-01-04 15:49:35 +0900

    spring boot setting for intellij -- study 2nd

---
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -0,0 +1,19 @@
+package hello.hellospring.domain;
+
+public class Member {
+    private Long id;
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+}

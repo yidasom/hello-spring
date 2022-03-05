902bf36ceafdd793d4eff8d91b61559c92ad5fd7
yidasom <somlh1212@gmail.com>
2022-03-06 00:40:29 +0900

    [20220305] aop sample

---
+++ src/main/java/hello/hellospring/aop/LogCheckAop.java
@@ -0,0 +1,28 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogCheckAop {
+
+//    @Pointcut("execution(* hello.hellospring..*(..))")
+//    private void pointCut() {
+//    }
+
+    // @Around("pointCut()")
+    @Around("execution(* hello.hellospring..*(..))")
+    public Object execute(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        try {
+            Object result = proceedingJoinPoint.proceed();
+            System.out.println(result);
+            return result;
+        } finally {
+            System.out.println("success ?!!!");
+        }
+
+    }
+}
